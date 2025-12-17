f53657edadade5033ed21630b50e4ffec124fd4b
cappscr <capps.christopher@gmail.com>
2025-12-16 21:36:36 -0500

    Adds an auth slice

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 
 import { type Post, postAdded } from './postsSlice'
-import { selectAllUsers } from '../users/usersSlice'
+import { selectCurrentUsername } from '../auth/authSlice'
 
 interface AddPostFormFields extends HTMLFormControlsCollection {
   postTitle: HTMLInputElement
@@ -18,7 +18,7 @@ interface AddPostFormElements extends HTMLFormElement {
 export const AddPostForm = () => {
   // Get the `dispatch` method from the store
   const dispatch = useAppDispatch()
-  const users = useAppSelector(selectAllUsers)
+  const userId = useAppSelector(selectCurrentUsername)!
 
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
@@ -27,18 +27,11 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
-    const userId = elements.postAuthor.value
 
     dispatch(postAdded(title, content, userId))
 
     e.currentTarget.reset()
   }
-
-  const userOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
 
   return (
     <section>
@@ -46,11 +39,6 @@ export const AddPostForm = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" defaultValue="" required />
-        <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" name="postAuthor" required>
-          <option value=""></option>
-          {userOptions}
-        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom'
 
 import { useAppSelector } from '@/app/hooks'
 import { selectPostById } from './postsSlice'
+import { selectCurrentUsername } from '../auth/authSlice'
 
 import { PostAuthor } from '../users/PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
@@ -11,6 +12,7 @@ export const SinglePostPage = () => {
   const { postId } = useParams()
 
   const post = useAppSelector((state) => selectPostById(state, postId!))
+  const currentUsername = useAppSelector(selectCurrentUsername)!
 
   if (!post) {
     return (
@@ -20,6 +22,8 @@ export const SinglePostPage = () => {
     )
   }
 
+  const canEdit = currentUsername === post.user
+
   return (
     <section>
       <article className="post">
@@ -28,9 +32,11 @@ export const SinglePostPage = () => {
         <p className="post-content">{post.content}</p>
         <TimeAgo timestamp={post.date} />
         <br />
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
+        {canEdit && (
+          <Link to={`/editPost/${post.id}`} className="button">
+            Edit Post
+          </Link>
+        )}
         <ReactionButtons post={post} />
       </article>
     </section>
