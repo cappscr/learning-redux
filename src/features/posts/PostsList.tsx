e314a988a770c9f49ff057b5863cefd6323c900f
cappscr <capps.christopher@gmail.com>
2025-12-21 05:30:36 -0500

    Updates the PostList and PostExcerpt components to utilize normalized state

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -8,13 +8,15 @@ import { TimeAgo } from '@/components/TimeAgo'
 
 import { PostAuthor } from '../users/PostAuthor'
 import { ReactionButtons } from './ReactionButtons'
-import { Post, fetchPosts, selectAllPosts, selectPostsError, selectPostsStatus } from './postsSlice'
+import { fetchPosts, selectPostIds, selectPostById, selectPostsError, selectPostsStatus } from './postsSlice'
 
 interface PostExcerptProps {
-  post: Post
+  postId: string
 }
 
-function PostExcerpt({ post }: PostExcerptProps) {
+function PostExcerpt({ postId }: PostExcerptProps) {
+  const post = useAppSelector((state) => selectPostById(state, postId))
+
   return (
     <article className="post-excerpt" key={post.id}>
       <h3>
@@ -33,7 +35,7 @@ function PostExcerpt({ post }: PostExcerptProps) {
 export const PostsList = () => {
   const dispatch = useAppDispatch()
   // Select the `state.posts` value from the store into the component
-  const posts = useAppSelector(selectAllPosts)
+  const orderedPostIds = useAppSelector(selectPostIds)
   const postStatus = useAppSelector(selectPostsStatus)
   const postsError = useAppSelector(selectPostsError)
 
@@ -48,10 +50,7 @@ export const PostsList = () => {
   if (postStatus === 'loading') {
     content = <Spinner text="Loading..." />
   } else if (postStatus === 'completed') {
-    // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-
-    content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />)
+    content = orderedPostIds.map((postId) => <PostExcerpt key={postId} postId={postId} />)
   } else if (postStatus === 'failed') {
     content = <div>{postsError}</div>
   }
